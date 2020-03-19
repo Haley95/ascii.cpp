06deca6dedea75ad540409a630ae6a4ac27acee4
haley <haleyhenson76@yahoo.com>
2020-03-18 20:48:51 -0500

    this is my second commit

--- ascii.cpp
+++ ascii.cpp
@@ -1,7 +1,372 @@
-#include <iostream>
+#include<iostream>
+#include <cstring>
 using namespace std;
-int main()
-{ cout <<                              "*******     ********  **     ** \n";
+
+int main(int argc, char** argv)
+{
+
+do
+{
+    if (argv[1][0] == 'A' && argv[1][0] != 'a')
+	{
+        
+   cout<<"    ****    \n";   
+   cout<<"   **//**   \n";  
+   cout<<"  **  //**  \n";
+   cout<<" ********** \n";
+   cout<<"/**//////** \n";
+   cout<<"/**     /** \n";
+   cout<<"//      //  \n";
+
+   cout << "ASCII Value of " << 'A' << " is: " << int(argc) << endl;
+  return 1;
+	}
+	if (argv[1][0] == 'B' && argv[1][0] != 'b')
+	{
+ cout<< "******   \n";  
+ cout<< "/*////** \n";
+ cout<< "/*   /** \n";
+ cout<< "/******  \n";
+ cout<< "/*//// **\n";
+ cout<< "/*    /**\n";
+ cout<< "/******* \n";
+ cout<< "///////  \n";
+
+
+cout << "ASCII Value of " << 'B' << " is: " << int(argc) << endl;
+return 1;
+	}
+ if (argv[1][0] == 'C' && argv[1][0] != 'c')
+	{
+cout<<      "******     \n"; 
+cout<<     "**////**    \n";
+cout<<   "**    //      \n";
+cout<< "/**             \n";
+cout<< "/**             \n";
+cout<<   "//******      \n";
+cout<<    "//////       \n"; 
+
+cout << "ASCII Value of " << 'C' << " is: " << int(argc) << endl;
+return 1;
+    }
+
+	if (argv[1][0] == 'D' && argv[1][0] != 'd')
+	{
+cout<<  "*******    \n";
+cout<<  "/**////**  \n";
+cout<<  "/**    /** \n";
+cout<<  "/**    /** \n";
+cout<<  "/**    /** \n";
+cout<<  "/**    **  \n";
+cout<<  "/*******   \n";
+cout<<  "///////    \n";
+
+cout << "ASCII Value of " << 'D' << " is: " << int(argc) << endl;
+return 1;
+	}
+	if (argv[1][0] == 'E' && argv[1][0] != 'e')
+	{
+cout<<  " ********   \n";
+cout<<  "/**/////    \n";
+cout<<  "/**		 \n";
+cout<<  "/*******    \n";
+cout<<  "/**////     \n";
+cout<<  "/**	     \n";
+cout<<  "/********   \n";
+cout<<  "////////    \n";
+
+cout << "ASCII Value of " << 'E' << " is: " << int(argc) << endl;
+return 1;
+	}
+
+	if (argv[1][0] == 'F' && argv[1][0] != 'f')
+	{
+cout<<  " ********   \n";
+cout<<  "/**/////    \n";
+cout<<  "/**		 \n";
+cout<<  "/*******    \n";
+cout<<  "/**////     \n";
+cout<<  "/**	     \n";
+cout<<  "/**   \n";
+cout<<  "//    \n";
+
+cout << "ASCII Value of " << 'F' << " is: " << int(argc) << endl;
+return 1;
+}
+
+	if (argv[1][0] == 'G' && argv[1][0] != 'g')
+	{
+cout<<  "********     \n";
+cout<<  "**//////**   \n";
+cout<<  "**      //   \n";
+cout<<  "/**          \n";
+cout<<  "/**    ***** \n";
+cout<<  "//**  ////** \n";
+cout<<  " //********  \n";
+cout<<  "  ////////   \n";
+
+cout << "ASCII Value of " << 'G' << " is: " << int(argc) << endl;
+return 1;
+}
+ 
+ 	if (argv[1][0] == 'H' && argv[1][0] != 'h')
+	{
+cout<<  "**      **   \n";
+cout<< "/**     /**   \n";
+cout<< "/**     /**   \n";
+cout<< "/**********   \n";
+cout<< "/**//////**   \n";
+cout<< "/**     /**   \n";
+cout<< "/**     /**   \n";
+cout<< "//      //    \n";
+
+cout << "ASCII Value of " << 'H' << " is: " << int(argc) << endl;
+return 1;
+}
+	if (argv[1][0] == 'I' && argv[1][0] != 'i')
+	{
+cout<<" ** \n";
+cout<<"/** \n";
+cout<<"/** \n";
+cout<<"/** \n";
+cout<<"/** \n";
+cout<<"/** \n";
+cout<<"/** \n";
+cout<<"//  \n";
+
+cout << "ASCII Value of " << 'I' << " is: " << int(argc) << endl;
+return 1;
+}
+   if (argv[1][0] == 'J' && argv[1][0] != 'j')
+	{
+cout<<     "** \n";
+cout<<    "/** \n";
+cout<<    "/** \n";
+cout<<    "/** \n";
+cout<<    "/** \n";
+cout<<"**  /** \n";
+cout<<"//***** \n";
+cout<<" /////  \n";
+
+cout << "ASCII Value of " << 'J' << " is: " << int(argc) << endl;
+return 1;
+}
+   if (argv[1][0] == 'K' && argv[1][0] != 'k')
+	{
+cout<< " **   **   \n";
+cout<< "/**  **    \n";
+cout<< "/** **     \n";
+cout<< "/****      \n";
+cout<< "/**/**     \n";
+cout<< "/**//**    \n";
+cout<< "/** //**   \n";
+cout<< "//   //    \n";
+
+cout << "ASCII Value of " << 'K' << " is: " << int(argc) << endl;
+return 1;
+}
+	if (argv[1][0] == 'L' && argv[1][0] != 'l')
+	{
+cout<<" **       \n";
+cout<<"/**       \n";
+cout<<"/**       \n";
+cout<<"/**       \n";
+cout<<"/**       \n";
+cout<<"/**       \n";
+cout<<"/******** \n";
+cout<<"////////  \n";
+
+
+cout << "ASCII Value of " << 'L' << " is: " << int(argc) << endl;
+return 1;
+}
+    if (argv[1][0] == 'M' && argv[1][0] != 'm')
+	{
+cout<< " ****     ****  \n";
+cout<< "/**/**   **/**  \n";
+cout<< "/**//** ** /**  \n";
+cout<< "/** //***  /**  \n";
+cout<< "/**  //*   /**  \n";
+cout<< "/**   /    /**  \n";
+cout<< "/**        /**  \n";
+cout<< "//         //   \n";
+
+cout << "ASCII Value of " << 'M' << " is: " << int(argc) << endl;
+return 1;
+}
+    if (argv[1][0] == 'O' && argv[1][0] != 'o')
+	{
+cout<< "   *******  \n";
+cout<< "  **/////** \n";
+cout<< " **     //**\n";
+cout<< "/**      /**\n";
+cout<< "/**      /**\n";
+cout<< "//**     ** \n";
+cout<< " //*******  \n";
+cout<< "  ///////   \n";
+
+cout << "ASCII Value of " << 'O' << " is: " << int(argc) << endl;
+return 1;
+}
+    if (argv[1][0] == 'P' && argv[1][0] != 'p')
+	{
+cout<< "*******   \n";
+cout<< "/**////** \n";
+cout<< "/**   /** \n";
+cout<< "/*******  \n";
+cout<< "/**////   \n";
+cout<< "/**       \n";
+cout<< "/**       \n";
+cout<< "//        \n";
+
+cout << "ASCII Value of " << 'P' << " is: " << int(argc) << endl;
+return 1;
+}
+     if (argv[1][0] == 'Q' && argv[1][0] != 'q')
+	{
+cout<< "  *******      \n";
+cout<< " **/////**     \n";
+cout<< "**     //**    \n";
+cout<< "/**      /**   \n";
+cout<< "/**    **/**   \n";
+cout<< "//**  // **    \n";
+cout<< " //******* **  \n";
+cout<< "  /////// //   \n";
+
+cout << "ASCII Value of " << 'Q' << " is: " << int(argc) << endl;
+return 1; 
+}
+    if (argv[1][0] == 'R' && argv[1][0] != 'r')
+	{
+cout<< " *******   \n";
+cout<< "/**////** \n";
+cout<< "/**   /** \n";
+cout<< "/*******  \n";
+cout<< "/**///**  \n";
+cout<< "/**  //** \n";
+cout<< "/**   //**\n";
+cout<< "//     // \n";
+
+cout << "ASCII Value of " << 'R' << " is: " << int(argc) << endl;
+return 1;
+}
+    if (argv[1][0] == 'S' && argv[1][0] != 's')
+	{
+cout<< " ********  \n";
+cout<< " **//////  \n";
+cout<< "/**        \n";
+cout<< "/********* \n";
+cout<< "////////** \n";
+cout<< "       /** \n";
+cout<< " ********  \n";
+cout<< "////////   \n";
+
+cout << "ASCII Value of " << 'S' << " is: " << int(argc) << endl;
+return 1;
+}
+    if (argv[1][0] == 'T' && argv[1][0] != 't')
+	{
+cout<<" **********\n";
+cout<<"/////**/// \n";
+cout<<"    /**    \n";
+cout<<"    /**    \n";
+cout<<"    /**    \n";
+cout<<"    /**    \n";
+cout<<"    /**    \n";
+cout<<"    //     \n";
+
+cout << "ASCII Value of " << 'T' << " is: " << int(argc) << endl;
+return 1;
+}
+    if (argv[1][0] == 'U' && argv[1][0] != 'u')
+	{
+cout<<" **     ** \n";
+cout<<"/**    /** \n";
+cout<<"/**    /** \n";
+cout<<"/**    /** \n";
+cout<<"/**    /** \n";
+cout<<"/**    /** \n";
+cout<<"//*******  \n";
+cout<<" ///////   \n";
+
+cout << "ASCII Value of " << 'U' << " is: " << int(argc) << endl;
+return 1;
+}
+    if (argv[1][0] == 'V' && argv[1][0] != 'v')
+	{
+cout<<" **      ** \n";
+cout<<"/**     /** \n";
+cout<<"/**     /** \n";
+cout<<"//**    **  \n";
+cout<<" //**  **   \n";
+cout<<"  //****    \n";
+cout<<"   //**     \n";
+cout<<"    //      \n";
+
+cout << "ASCII Value of " << 'V' << " is: " << int(argc) << endl;
+return 1;
+}
+    if (argv[1][0] == 'W' && argv[1][0] != 'w')
+	{
+cout<<" **       **\n";
+cout<<"/**      /**\n";
+cout<<"/**   *  /**\n";
+cout<<"/**  *** /**\n";
+cout<<"/** **/**/**\n";
+cout<<"/**** //****\n";
+cout<<"/**/   ///**\n";
+cout<<"//       // \n";
+
+cout << "ASCII Value of " << 'W' << " is: " << int(argc) << endl;
+return 1;
+}
+    if (argv[1][0] == 'X' && argv[1][0] != 'x')
+	{
+cout<<" **     **\n";
+cout<<"//**   ** \n";
+cout<<" //** **  \n";
+cout<<"  //***   \n";
+cout<<"   **/**  \n";
+cout<<"  ** //** \n";
+cout<<" **   //**\n";
+cout<<"//     // \n";
+
+cout << "ASCII Value of " << 'X' << " is: " << int(argc) << endl;
+return 1;
+}
+    if (argv[1][0] == 'Y' && argv[1][0] != 'y')
+	{
+cout<<" **    **\n";
+cout<<"//**  ** \n";
+cout<<" //****  \n";
+cout<<"  //**   \n";
+cout<<"   /**   \n";
+cout<<"   /**   \n";
+cout<<"   /**   \n";
+cout<<"   //    \n";
+
+cout << "ASCII Value of " << 'Y' << " is: " << int(argc) << endl;
+return 1;
+}
+    if (argv[1][0] == 'Z' && argv[1][0] != 'z')
+	{
+cout<<" ********\n";
+cout<<"//////** \n";
+cout<<"     **  \n";
+cout<<"    **   \n";
+cout<<"   **    \n";
+cout<<"  **     \n";
+cout<<" ********\n";
+cout<<"//////// \n";
+
+cout << "ASCII Value of " << 'Z' << " is: " << int(argc) << endl;
+return 1;
+}
+  
+
+ } while(false);
+         {
+  cout <<                              "*******     ********  **     ** \n";
   cout <<                             "**/////**   **//////  /**    /** \n";
   cout <<                            "**     //** /**        /**    /** \n";  
   cout <<                           "/**      /** /********* /**    /** \n";  
@@ -9,5 +374,8 @@ int main()
   cout <<                           "//**     **         /** /**    /** \n";
   cout <<                            "//*******    ********  //*******  \n";
   cout <<                              "///////   ////////    ///////   \n";
-return 0;
-}
+         }
+    
+    return 0;
+  }
+
